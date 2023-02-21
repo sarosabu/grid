ad29d9ad5ae3dff591b3989b2df3728c7693e2a1
sarosabu <115157608+sarosabu@users.noreply.github.com>
2023-02-21 20:53:17 +0530

    Add files via upload

---
+++ crud operation/crud.js
@@ -0,0 +1,64 @@
+Students=[{
+    "Title Of Courtesy":"Mr.",
+    "StudentId":1,
+    "StudentName":"Saravanan.G",
+    "BirthDate": "2002-07-02",
+    "Address":"124/6 sakthivelnagar,seetharampalayam,Tiruchengode",
+    "JoiningDate": "2022-07-02",
+    "Percentage":78
+},
+{
+    "Title Of Courtesy":"Ms.",
+    "StudentId":2,
+    "StudentName":"Vaishnavi.V",
+    "BirthDate": "2002-09-10",
+    "Address":"12/6 pallipalayam,Erode",
+    "JoiningDate": "2022-07-02",
+    "Percentage":86
+},
+{
+    "Title Of Courtesy":"Mr.",
+    "StudentId":3,
+    "StudentName":"NaveenKumar.P",
+    "BirthDate": "2002-07-02",
+    "Address":"24/8 Kallipatti",
+    "JoiningDate": "2022-07-02",
+    "Percentage":90
+},
+{
+    "Title Of Courtesy":"Mr.",
+    "StudentId":4,
+    "StudentName":"Sathasivam.T",
+    "BirthDate": "2002-05-02",
+    "Address":"12B, thondikaradu,Tiruchengode",
+    "JoiningDate": "2022-07-02",
+    "Percentage":87
+},
+{
+    "Title Of Courtesy":"Mr.",
+    "StudentId":5,
+    "StudentName":"Karthikeyan.K",
+    "BirthDate": "2002-08-20",
+    "Address":"124B ELectronicCity,Bengaluru",
+    "JoiningDate": "2022-07-02",
+    "Percentage":96
+},
+{
+    "Title Of Courtesy":"Ms.",
+    "StudentId":6,
+    "StudentName":"Akila.R",
+    "BirthDate": "2002-04-02",
+    "Address":"13B, whiteBoard,Bengaluru",
+    "JoiningDate": "2022-07-02",
+    "Percentage":70
+},
+{
+    "Title Of Courtesy":"Ms.",
+    "StudentId":7,
+    "StudentName":"Sneha.P",
+    "BirthDate": "2002-06-22",
+    "Address":"12/8, Osapatti Thalakuttaipudur Anthiyur",
+    "JoiningDate": "2022-10-02",
+    "Percentage":98
+}
+]
